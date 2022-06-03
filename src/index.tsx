29d50bce53aa870a2a79905eca43909141d796bc
foxsheldin <fruktsky@gmail.com>
2022-06-03 10:51:32 +0300

    Настройка Redux

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="nfd-admin">
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter basename="nfd-admin">
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
