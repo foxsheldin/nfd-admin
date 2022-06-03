171e7f47da90456a8afc07d3e60d9ac5770e943e
foxsheldin <fruktsky@gmail.com>
2022-06-03 11:16:49 +0300

    V-07 настройка роутов

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Outlet, Route, Routes } from "react-router-dom";
 import AuthPage from "./pages/AuthPage/AuthPage";
 import "./assets/scss/global.scss";
+import OrdersPage from "./pages/OrdersPage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<AuthPage />} />
+      <Route index element={<AuthPage />} />
+      <Route path="admin" element={<AuthPage />} />
+      <Route path="admin-panel" element={<Outlet />}>
+        <Route path="orders" element={<OrdersPage />} />
+        <Route path="*" element={"Error 404"} />
+      </Route>
     </Routes>
   );
 }
